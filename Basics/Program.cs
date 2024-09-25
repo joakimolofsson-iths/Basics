663ceff7a7575549813d2e788146310f397e52f0
Joakim Olofsson <joakim.olofsson@iths.se>
2024-09-25 14:36:30 +0200

    More exercies done!

--- Basics/Program.cs
+++ Basics/Program.cs
@@ -1,10 +1,12 @@
-﻿namespace Basics
+﻿using System.Threading.Channels;
+
+namespace Basics
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            int myAge = 37;
+            /*int myAge = 37;
             string myName = "Joakim";
             Console.WriteLine($"My name is {myName} and I'm {myAge} years old!");
             
@@ -18,15 +20,15 @@
             float flo = 5.5f;
             decimal dec = 5.5m;
 
-            Console.WriteLine("Ange pris exklusive moms:");
+            Console.WriteLine("\nAnge pris exklusive moms:");
             int prisExMoms = int.Parse(Console.ReadLine());
             decimal prisMedMoms = prisExMoms + (prisExMoms * 0.06m);
             Console.WriteLine($"Pris inklusive moms: {prisMedMoms}");
             //Annan lösning
-            /*decimal prisExMoms = decimal.Parse(Console.ReadLine());
-            decimal prisMedMoms = prisExMoms + (prisExMoms * 0.06m);*/
+            *//*decimal prisExMoms = decimal.Parse(Console.ReadLine());
+            decimal prisMedMoms = prisExMoms + (prisExMoms * 0.06m);*//*
 
-            Console.WriteLine("Räkna ut omkretsen på en rektangel.\nLängd:");
+            Console.WriteLine("\nRäkna ut omkretsen på en rektangel.\nLängd:");
             double langd = double.Parse(Console.ReadLine());
             Console.WriteLine("Bredd:");
             double bredd = double.Parse(Console.ReadLine());
@@ -34,6 +36,174 @@
             Console.WriteLine($"Omkrets: {omkrets}");
             double area = langd * bredd;
             Console.WriteLine($"Area: {area}");
+
+            int number = 10;
+            if(number > 10)
+            {
+                Console.WriteLine("Bigger then 10!");
+            }
+            else if(number < 10)
+            {
+                Console.WriteLine("Smaller then 10!");
+            }
+            else
+            {
+                Console.WriteLine("Equal to 10!");
+            }
+
+            int number2 = 20;
+            switch(number2)
+            {
+                case 10:
+                    Console.WriteLine("10");
+                    break;
+                case 15:
+                    Console.WriteLine("15");
+                    break;
+                case 20:
+                    Console.WriteLine("20");
+                    break;
+                default:
+                    Console.WriteLine("Default");
+                    break;
+            }
+
+            int number3 = 5;
+            string helloNumber = number3 > 5 ? "Bigger" : "Smaller";
+            Console.WriteLine(helloNumber);
+
+            string food1 = "Spagetti";
+            string food2 = "Hamburger";
+            string food3 = "Pancakes";
+            Console.WriteLine($"\nWhat would you like to eat?\n1. {food1}\n2. {food2}\n3. {food3}");
+            switch(Console.ReadLine())
+            {
+                case "1":
+                    Console.WriteLine($"You picked {food1.ToLower()}!");
+                    break;
+                case "2":
+                    Console.WriteLine($"You picked {food2.ToLower()}!");
+                    break;
+                case "3":
+                    Console.WriteLine($"You picked {food3.ToLower()}!");
+                    break;
+                default:
+                    Console.WriteLine("You didn't pick your food!");
+                    break;
+            }
+
+            int loopNum = 0;
+            while(loopNum < 10)
+            {
+                if(loopNum == 0)
+                {
+                    Console.WriteLine("\n");
+                }
+
+                loopNum++;
+                Console.WriteLine(loopNum);
+            }
+
+            loopNum = 0;
+            do
+            {
+                if (loopNum == 0)
+                {
+                    Console.WriteLine("\n");
+                }
+
+                loopNum++;
+                Console.WriteLine(loopNum);
+            }
+            while (loopNum < 10);
+
+            for(int i = 0; i < 10; i++)
+            {
+                if (i == 0)
+                {
+                    Console.WriteLine("\n");
+                }
+
+                Console.WriteLine(i + 1);
+            }*/
+
+            /*Random random = new Random();
+            int randomNum = random.Next(1, 10);
+            while(true)
+            {
+                Console.WriteLine("Guess a number between 1 - 10!");
+                int userGuess;                
+                bool parseUserGuess = int.TryParse(Console.ReadLine(), out userGuess);
+
+                if(parseUserGuess)
+                {
+                    if (userGuess == randomNum)
+                    {
+                        Console.WriteLine("Correct!");
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Wrong, guess again!");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Type a number!");
+                }                
+            }*/
+
+            /*while (true)
+            {
+                Console.WriteLine("Type a number!");
+                int userNumber;
+                bool parseUserInput = int.TryParse(Console.ReadLine(), out userNumber);
+
+                if (parseUserInput)
+                {
+                    if (userNumber > 0)
+                    {
+                        for(int i = 1; i < userNumber; i++)
+                        {
+                            Console.WriteLine(i);
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("The number needs to be larger than 0!");
+                    }
+                }
+            }*/
+
+            /*Random random = new Random();
+            int randomNum = random.Next(1, 50);
+            int guessesLeft = 10;
+
+            Console.WriteLine("Guess a number between 1 -50!");
+            for(int i = guessesLeft; i > 0; i--)
+            {
+                int userNumber;
+                bool parseUserInput = int.TryParse(Console.ReadLine(), out userNumber);
+
+                if(parseUserInput)
+                {
+                    if(userNumber == randomNum)
+                    {
+                        Console.WriteLine("Correct!");
+                        break;
+                    }
+                    else
+                    {
+                        if(i == 1)
+                        {
+                            Console.WriteLine($"Game Over!");
+                            break;
+                        }
+
+                        Console.WriteLine($"Wrong! You have {i - 1} guess left!");
+                    }
+                }
+            }*/
         }
     }
 }
